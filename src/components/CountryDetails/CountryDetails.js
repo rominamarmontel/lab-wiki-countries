ecb0701ab2d3733b3db6c44a3ca1b398bd85bffa
romi <romi.varnier@gmail.com>
2023-03-03 16:32:39 +0100

    Iteration 4 Done

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const CountryDetails = ({ countries }) => {
-  const [country, setCountry] = useState([]);
   const { code } = useParams();
   const foundCountry = countries.find((country) => {
     return country.alpha3Code === code;
   });
-  // const url = 'https://ih-countries-api.herokuapp.com/countries';
 
-  // useEffect(() => {
-  //   fetch(`${url}/${alpha3Code}`)
-  //     .then((raw) => {
-  //       return raw.json();
-  //     })
-  //     .then((res) => {
-  //       // console.log(res);
-  //       setCountry(res[0]);
-  //     });
-  // }, [alpha3Code]);
-  console.log(foundCountry);
   return (
     <div className="col-7">
       <h1>Hello</h1>
@@ -41,13 +28,20 @@ const CountryDetails = ({ countries }) => {
           <tr>
             <td>Borders</td>
             <td>
-              {foundCountry.borders.map((border) => {
-                <ul>
-                  <li>
-                    return <Link to={`/${border}`}>{border}</Link>;
-                  </li>
-                </ul>;
-              })}
+              <ul>
+                {foundCountry.borders.map((border) => {
+                  const borderCountry = countries.find((country) => {
+                    return country.alpha3Code === border;
+                  });
+                  return (
+                    <li>
+                      <Link to={`/${border}`}>
+                        {borderCountry.altSpellings[2]}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </td>
           </tr>
         </tbody>
